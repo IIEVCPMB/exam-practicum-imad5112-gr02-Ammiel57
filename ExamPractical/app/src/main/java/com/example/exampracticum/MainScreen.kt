b5987e3f83d57113e48055ad5bc8dcd431b4c3f6
Ammiel57 <ST10470753@myemeris.edu.za>
2026-06-10 11:31:31 +0200

    main screen
    code for main screen

--- ExamPractical/app/src/main/java/com/example/exampracticum/MainScreen.kt
+++ ExamPractical/app/src/main/java/com/example/exampracticum/MainScreen.kt
@@ -11,6 +11,13 @@ class MainScreen : AppCompatActivity()
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
     setContentView(R.layout.main_screen)
+
+    val edtGear1 = findViewById (R.id.edtGear1)
+    val edtGear2 = findViewById(R.id.edtGear2)
+    val edtGear3 = findViewById(R.id.edtGear3)
+    val edtGear4 = findViewById(R.id.edtGear4)
+
+
     ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
         val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
         v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
