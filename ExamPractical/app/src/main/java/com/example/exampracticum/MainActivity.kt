335a436b0316f10641941b71c76b6318ad69bf39
Ammiel57 <ST10470753@myemeris.edu.za>
2026-06-10 09:42:11 +0200

    Main Screen
    Added the main screen

--- ExamPractical/app/src/main/java/com/example/exampracticum/MainActivity.kt
+++ ExamPractical/app/src/main/java/com/example/exampracticum/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.exampracticum
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Looper
+import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +14,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        //Handler is used to delay the next action
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            //create an intent to move from one activity to the register activity
+            val intent = Intent(this, MainScreen::class.java)
+            startActivity(intent)
+
+        },3000) //3000 milliseconds = 3 seconds
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
---
+++ ExamPractical/app/src/main/java/com/example/exampracticum/MainScreen.kt
@@ -0,0 +1,4 @@
+package com.example.exampracticum
+
+class MainScreen {
+}
